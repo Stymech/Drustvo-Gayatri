93e1b570586b66793eb959ddce5aaf7518234da0
Gapa00 <gs7329@student.uni-lj.si>
2024-12-10 15:24:17 +0100

    Premaknjen repo

--- app.js
+++ app.js
@@ -1,62 +1,60 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const besediloElement = document.querySelector('.besedilo');
+    const textElement = document.querySelector('.text-center');
 
-// ============================= DISSOLVE TITLE ==============================
+    besediloElement.addEventListener('click', function(event) {
+        consol.log('Besedilo je bilo kliknjeno');
+        const clickedElement = event.target;
 
-window.onscroll = function() {dissolve()};
+        // Check if the clicked element is a link or contains a link
+        if (clickedElement.tagName === 'A' || clickedElement.closest('a')) {
+            console.log('Link was clicked');
+            // Add your code here to handle link click
+        } else {
+            console.log('Text was clicked');
+            // Add your code here to handle text click
+        }
+    });
+    besediloElement.addEventListener('text', function(event) {
+        consol.log('Text je bilo kliknjeno');
+        const clickedElement = event.target;
 
-function dissolve() {
-    let scrollPosition = window.pageYOffset;
-    // console.log(scrollPosition);
-
-    if (scrollPosition > 600 || (scrollPosition > 300 && screen.width < 600)) {
-        target = document.getElementById('target');
-        target.classList.add('clicked');
-    }
-    else {
-        target = document.getElementById('target');
-        target.classList.remove('clicked');
-    }
-}
-
-// ================================ OPIS OSEB =================================
-
-let imena = document.querySelectorAll('h2');
-let opisi = document.querySelectorAll('.opisOsebe')
-
-// imena.forEach(el => el.addEventListener('click', naslov));   	
-imena.forEach(el => el.addEventListener('click', toggleText));
-opisi.forEach(el => el.addEventListener('click', toggleText));
+        // Check if the clicked element is a link or contains a link
+        if (clickedElement.tagName === 'A' || clickedElement.closest('a')) {
+            console.log('Link was clicked');
+            // Add your code here to handle link click
+        } else {
+            console.log('Text was clicked');
+            // Add your code here to handle text click
+        }
+    });
+});
 
 
-function naslov(id, zakasnitev) {
+// Define latitude, longitude and zoom level
+const latitude = 46.19455195346911;
+const longitude = 13.727996349334717;
+const zoom = 13;
 
-    if (zakasnitev === true) {
-        setTimeout(function(){imena[id].classList.toggle('clicked');}, 500);
-    }
-    else {
-        imena[id].classList.toggle('clicked');
-    }
-}
+var mymap = L.map('mapWrap');
 
-function ponastavi(event) {
-    // alert(event.target)
-    toggleText(event)
-}
+// Add initial marker & popup window
+var mmr = L.marker([0,0]);
+mmr.bindPopup('0,0');
+mmr.addTo(mymap);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
+    foo: 'bar',
+    attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'}
+).addTo(mymap);
 
-function toggleText(event) {
-    
-    var id = event.target.id;
+// Set lat lng position and zoom level of map 
+mmr.setLatLng(L.latLng(latitude, longitude));
+mymap.setView([latitude, longitude], zoom);
 
-    if (opisi[id].classList.contains('activeBesedilo')) {
-        opisi[id].classList.toggle('activeBesedilo'); 
-        naslov(id, true)
-        // setTimeout(function() {opisi[id].classList.toggle('active')}, 500);
-        // opisi[id].style.opacity = "0";
-    }
-    else {            
-        naslov(id, false);          
-        setTimeout(function(){opisi[id].classList.toggle('activeBesedilo');}, 500); 
-        // opisi[id].classList.toggle('active');                                  
-        // opisi[id].style.opacity = "1";
-        // setTimeout(function() {opisi[id].classList.toggle('active')}, 500);
-    }
+// Set marker onclick event
+mmr.on('click', openPopupWindow);
+
+// Marker click event handler
+function openPopupWindow(e) {
+    mmr.setPopupContent('Zatolmin 59, 5220 Tolmin').openPopup();
 }
